5d8ce159f923fb7e9a25dca0f06f13f0b9a5cf9d
ihyelim <qazxcv0691@naver.com>
2025-04-28 00:37:40 +0900

    refactor: input 렌더링을 innerHTML 대신 createElement 방식으로 변경

--- src/components/input.ts
+++ src/components/input.ts
@@ -1,6 +1,12 @@
 export function renderInput(): HTMLElement {
 	const container = document.createElement('div');
-	container.innerHTML = `<input id="todo-input" type="text" placeholder="What needs to be done?" />`;
+
+	const input = document.createElement('input');
+	input.id = 'todo-input';
+	input.type = 'text';
+	input.placeholder = 'What needs to be done?';
+
+	container.appendChild(input);
 
 	return container;
 }
